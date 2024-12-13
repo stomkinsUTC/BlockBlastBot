498967e257ded09e967fbd0bdb6d36782274adaa
Stephen Tomkins <stomkins@utcsheffield.org.uk>
2024-12-13 11:10:54 +0000

    Add project files.

---
+++ BlockBlastBot/BBBCore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBlastBot
+{
+    internal static class BBBCore
+    {
+        //Board format: True = piece, False = empty
+        public static List<List<bool>> gameBoard
+        {
+            get { return gameBoard; }
+            set { gameBoard = value; }
+        }
+
+        //Index - Item, Row, Col
+        public static List<List<List<bool>>> gamePieces = new List<List<List<bool>>>();
+        
+    }
+}
